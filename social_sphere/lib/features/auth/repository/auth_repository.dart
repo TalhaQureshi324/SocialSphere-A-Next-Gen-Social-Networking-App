0d80e93e76c890cba82c4b282af99c6cb26ed30c
AbdullahhussainYasim <abdullahhussainyasim1@gmail.com>
2025-04-14 14:50:19 +0500

    Storing data to firesotre

--- social_sphere/lib/features/auth/repository/auth_repository.dart
+++ social_sphere/lib/features/auth/repository/auth_repository.dart
@@ -1,55 +1,74 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:social_sphere/core/providers/firebase_providers.dart';
+import 'package:social_sphere/models/user_model.dart';
+import 'package:social_sphere/core/constants/constants.dart';
+import 'package:social_sphere/core/constants/firebase_constants.dart';
 
+final authRepositoryProvider = Provider(
+  (ref) => AuthRepository(
+    firestore: ref.read(firestoreProvider),
+    auth: ref.read(authProvider),
+    googleSignIn: ref.read(googleSignInProvider),
+  ),
+);
 
-
-
-
-final authRepositoryProvider = Provider((ref) => AuthRepository(firestore: ref.read(firestoreProvider),auth: ref.read(authProvider),googleSignIn: ref.read(googleSignInProvider),),);
-
-class AuthRepository
-{
+class AuthRepository {
   final FirebaseFirestore _firestore;
   final FirebaseAuth _auth;
   final GoogleSignIn _googleSignIn;
 
-
-  AuthRepository({required FirebaseFirestore firestore, required FirebaseAuth auth, required GoogleSignIn googleSignIn})
-      :  _auth = auth,
+  AuthRepository({
+    required FirebaseFirestore firestore,
+    required FirebaseAuth auth,
+    required GoogleSignIn googleSignIn,
+  }) : _auth = auth,
        _firestore = firestore,
-        _googleSignIn = googleSignIn;
+       _googleSignIn = googleSignIn;
+
+  CollectionReference get _users =>
+      _firestore.collection(FirebaseConstants.usersCollection);
+
+  void SignInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+
+      final googleAuth = await googleUser?.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      UserCredential userCredential = await _auth.signInWithCredential(
+        credential,
+      );
+
+      UserModel userModel;
 
 
+      if(userCredential.additionalUserInfo!.isNewUser) {
+        userModel = UserModel(
+          name: userCredential.user!.displayName ?? 'No Name',
+          profilePic: userCredential.user!.photoURL ?? Constants.avatarDefault,
+          banner: Constants.bannerDefault,
+          uid: userCredential.user!.uid,
+          isAuthenticated: true,
+          karma: 0,
+          awards: [],
+        );
+      await _users.doc(userCredential.user!.uid).set(userModel.toMap());
 
-        void SignInWithGoogle() async
-        {
-          try{
-            final  GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-
-            final googleAuth = await googleUser?.authentication;
-
-            final credential = GoogleAuthProvider.credential(
-              accessToken: googleAuth?.accessToken,
-              idToken: googleAuth?.idToken,
-            );
-
-
-            UserCredential userCredential = await _auth.signInWithCredential(credential);
+      } 
 
 
-            print(userCredential.user?.email);
+      
 
-
-
-          }
-          catch(e)
-          {
-            print(e);
-          }
-
-        }
-
+    } catch (e) {
+      print(e);
+    }
+  }
 }
